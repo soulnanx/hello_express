1698675764eb2d8f3eeadd3460f4f292aac7a24f
renan <rsantos.dev@gmail.com>
2016-06-27 23:01:17 -0300

    adicao do atributo gender em people

--- app/models/person.js
+++ app/models/person.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
-var Person = mongoose.model('Person', { name: String, age: Number});
+var Person = mongoose.model('Person', { name: String, age: Number, gender: String});
 
 Person.prototype.toJSON = function(){
-	return {id: this._id, name: this.name, age: this.age};
+	return {id: this._id, name: this.name, age: this.age, gender: this.gender};
 }
 
 module.exports = Person;
